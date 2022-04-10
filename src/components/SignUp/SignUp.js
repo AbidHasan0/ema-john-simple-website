08d0cf02c8ca8f5f3e2eac9fc5ec3d8a44098b21
AbidHasan0 <abidhasanrazvi@gmail.com>
2022-04-11 01:41:36 +0600

    add to signup feild

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,25 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
+   const [confirmPassword, setConfirmPassword] = useState('');
+   const [error, setError = useState('');
+
+
+   const handleEmailBlur = (event) => {
+      setEmail(event.target.value);
+   }
+
+   const handlePasswordBlur = (event) => {
+      setPassword(event.target.value);
+   }
+
+   const handleConfirmPasswordBlur = (event) => {
+      setConfirmPassword(event.target.value)
+   }
+
+   const handleCreateUser = (event) => {
+      event.preventDefault();
+      if (password !== confirmPassword) {
+         setError('Two password did not match')
+         return;
+      }
+
+   }
+
    return (
       <div className='form-container'>
          <div>
             <h2 className='form-title'>Sign Up</h2>
-            <form action="">
+            <form onSubmit={handleCreateUser}>
                <div className='input-group'>
                   <label htmlFor="email">Email</label>
-                  <input type="email" name="email" id="" />
+                  <input onBlur={handleEmailBlur} type="email" name="email" id="" required />
 
                </div>
                <div className='input-group'>
                   <label htmlFor="password">Password</label>
-                  <input type="password" name="password" id="" />
+                  <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
 
                </div>
+               <div className='input-group'>
+                  <label htmlFor="confirm-password">Confirm Password</label>
+                  <input onBlur={handleConfirmPasswordBlur} type="password" name="confirm-password" id="" required />
+
+               </div>
+               <p style={{ color: 'red' }}>{error}</p>
                <input className='form-submit' type="submit" value="Sign Up" />
                <p>
-                  New to Ema-john?<Link className='form-link' to='/signup'>Create an Account</Link>
+                  Already Have an account?<Link className='form-link' to='/login'>Login</Link>
                </p>
             </form>
          </div>
